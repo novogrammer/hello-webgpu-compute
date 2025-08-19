2e887bbf164606765b2748a8f14104ec0df87dfb
Yusuke Kawamoto <novogrammer@gmail.com>
2025-08-19 14:46:52 +0900

    distの指定も変える必要があった。

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,8 @@ export default defineConfig({
   root: 'src',
   publicDir: '../public',
   build: {
+    outDir:'../dist',
+    emptyOutDir:true,
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'src/index.html'),
